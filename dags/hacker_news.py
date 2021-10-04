0d499fdcb876b64bf10577d5fe7db08c50374899
Madina <madina.gafarova@yandex.ru>
2021-10-04 22:50:22 +0300

    Add logging

--- dags/hacker_news.py
+++ dags/hacker_news.py
@@ -1,6 +1,6 @@
 from typing import List
 import logging
-import sys
+import logging.config
 
 from airflow.decorators import dag, task    # type: ignore
 from airflow.utils.dates import days_ago    # type: ignore
@@ -11,11 +11,33 @@ from src.api_service import (
         )   # type: ignore
 from src.parse_data import build_stories_info   # type: ignore
 
+logger = logging.getLogger('hackerNews')
+
+logging.config.dictConfig(
+    {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+            'console': {
+                'level': 'INFO',
+                'class': 'logging.StreamHandler'
+            }
+        },
+        'loggers': {
+            'hackerNews': {
+                'handlers': ['console']
+            },
+            'parseStoriesInfo': {
+                'handlers': ['console']
+            }
+        }
+    }
+)
+
 DEFAULT_ARGS = {
     'owner': 'airflow',
-    'schedule_interval': '@hourly'
+    'schedule_interval': None
 }
-logging.info(sys.path)
 
 @dag(
     dag_id='hacker_news',
@@ -25,22 +47,27 @@ logging.info(sys.path)
 def hacker_news():
     @task()
     def fetch_story_ids() -> List[int]:
+        logger.info('Get best stories from hacker news API')
+
         story_ids = get_beststories()
+        
         return story_ids
 
     @task()
     def process_stories_data(**context) -> str:
+        logger.info('Load data from xCom')
         ti = context['ti']
         data = ti.xcom_pull(task_ids='fetch_story_ids')
 
+        logger.info('Generate stories data')
         stories_generator = fetch_story_data(data)
-
         stories_info = build_stories_info(stories_generator)
 
         return stories_info
 
     @task()
     def save_data():
+        logger.info('Save stories data to minio')
         pass
 
 
--- src/parse_data.py
+++ src/parse_data.py
@@ -1,9 +1,12 @@
 import json
+import logging
 from typing import Generator, Union, List, Dict
 from datetime import datetime
 
+logger = logging.getLogger('parseStoriesInfo')
 
 def build_stories_info(stories_generator: Generator[dict, None, None]) -> str:
+    logger.info('Parse stories data')
     stories_info: Dict[str, Union[datetime, List[dict]]] = dict()
 
     now = datetime.now()    
@@ -12,6 +15,7 @@ def build_stories_info(stories_generator: Generator[dict, None, None]) -> str:
     stories_info['articles'] = list()
 
     for item in stories_generator:
+        logger.debug(f"Try to parse story {item['title']}")
         story = dict()
         story['rating'] = item['score']
         story['name'] = item['title']
