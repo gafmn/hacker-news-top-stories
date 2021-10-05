e7ad01db5e8d1c9615e971780f35eb5065270e01
Madina <madina.gafarova@yandex.ru>
2021-10-05 10:25:53 +0300

    Move URL to constant

--- src/api_service.py
+++ src/api_service.py
@@ -2,14 +2,16 @@ from typing import List, Generator
 
 import requests
 
+HACKER_NEWS_URL = 'https://hacker-news.firebaseio.com/v0/'
+
 
 def get_beststories() -> List[int]:
-    response = requests.get('https://hacker-news.firebaseio.com/v0/beststories.json')
+    response = requests.get(HACKER_NEWS_URL + 'beststories.json')
     result = response.json()
     return result
 
 
 def fetch_story_data(story_ids) -> Generator[dict, None, None]:
     for story_id in story_ids:
-        response = requests.get(f'https://hacker-news.firebaseio.com/v0/item/{story_id}.json')
+        response = requests.get(HACKER_NEWS_URL + f'item/{story_id}.json')
         yield response.json()
